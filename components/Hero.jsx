9dd2e77778710a30c1c6705bafbbabd4e70e1ae0
Artem Hrebeniuk <mac@MacBook-Pro-Artem.local>
2025-10-14 10:08:45 +0200

    fix

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -46,14 +46,33 @@ const Hero = memo(() => {
 			</picture>
 			<div className='px-4 pt-16 pb-10 sm:px-10 md:px-16 lg:py-28 2xl:py-48 md:pb-10 z-10 flex justify-between'>
 				<div className='relative max-w-[910px]'>
-					<h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold mb-6 sm:mb-8 md:mb-11'>
-						Serwis mobilnej wulkanizacji w Opolu !
+					<h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight mb-6 sm:mb-8 md:mb-10'>
+						Mobilna wulkanizacja w&nbsp;Opolu
 					</h1>
-					<p className='mb-10 sm:mb-11'>
-						Nie musisz czekać na wolny termin w tradycyjnym warsztacie ani stać
-						w korkach – Nasz mobilny serwis przyjedzie do ciebie w ciągu 60
-						minut.
+
+					<p className='text-base sm:text-lg md:text-xl leading-snug text-white/90 mb-6 sm:mb-8 md:mb-10'>
+						<strong>Wymiana kół i&nbsp;opon z&nbsp;wyważeniem.</strong>
+						<br />
+						<span className='text-secondary-orange font-semibold'>
+							Darmowy dojazd do klienta!
+						</span>
+						<br />
+						Przyjedziemy tam, gdzie jesteś, w&nbsp;ciągu{' '}
+						<strong>60&nbsp;minut</strong>.
+						<br />
+						<span className='text-white/90'>
+							💰 <strong>Cena</strong> jak w&nbsp;warsztacie.
+						</span>
 					</p>
+
+					<p className='text-base sm:text-lg md:text-xl font-semibold mb-10 sm:mb-11'>
+						To po co się&nbsp;męczyć?{' '}
+						<a href={LINKS.PHONE_TEL} className='text-secondary-orange'>
+							Zadzwoń!
+						</a>{' '}
+						📞
+					</p>
+
 					<div className='flex flex-col gap-2 sm:flex-row sm:gap-5'>
 						<Button
 							onClick={e => handleClick(e, 'reservation')}

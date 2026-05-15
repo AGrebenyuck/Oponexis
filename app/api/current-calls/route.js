32e314d02ece34b0f4610a9f92f69336783a5188
Artem Hrebeniuk <mac@MacBook-Pro-Artem.local>
2026-05-15 11:56:56 +0200

    minor improvements

--- app/api/current-calls/route.js
+++
@@ -1,6 +0,0 @@
-import { getMember } from '@/actions/zadarma'
-import { getCurrentCalls } from '../zadarma-webhook/route'
-
-export async function GET() {
-	return Response.json(getCurrentCalls())
-}

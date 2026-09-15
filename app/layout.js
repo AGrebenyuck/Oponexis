0564d80e5fa3930e9d6d6bf774c1ac754ee0aff9
Artem Hrebeniuk <mac@MacBook-Pro-Artem.local>
2026-09-15 23:58:47 +0200

    Load Cookiebot before Google tags

--- app/layout.js
+++ app/layout.js
@@ -125,19 +125,20 @@ export default function RootLayout({ children }) {
 			<head>
 				<link rel='icon' href='/siteIcon/favicon.ico' type='image/x-icon' />
 				<link rel='apple-touch-icon' href='/siteIcon/icon.svg'></link>
-				<Script
+				{/* Cookiebot requires its auto-blocking loader to be the first script. */}
+				{/* eslint-disable-next-line @next/next/no-sync-scripts */}
+				<script
 					id='Cookiebot'
 					src='https://consent.cookiebot.com/uc.js'
 					data-cbid={COOKIEBOT_CBID}
 					data-blockingmode='auto'
-					strategy='beforeInteractive'
+					type='text/javascript'
 				/>
-				<Script
+				<script
 					id='cookiebot-consent-default'
 					data-cookieconsent='ignore'
-					strategy='beforeInteractive'
-				>
-					{`
+					dangerouslySetInnerHTML={{
+						__html: `
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('consent', 'default', {
@@ -152,8 +153,9 @@ gtag('consent', 'default', {
 });
 gtag('set', 'ads_data_redaction', true);
 gtag('set', 'url_passthrough', false);
-`}
-				</Script>
+						`,
+					}}
+				/>
 				{GTM_ID ? (
 					<Script id='gtm-base' data-cookieconsent='ignore' strategy='afterInteractive'>
 						{`
